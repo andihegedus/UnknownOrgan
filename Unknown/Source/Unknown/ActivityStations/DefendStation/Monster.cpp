0eaf513fa1fadae348a5846fcf0fd24404a08c6d
andihegedus <74625948+andihegedus@users.noreply.github.com>
2025-01-17 23:35:55 -0800

    Add base class and test objects for monster functionality

---
+++ Unknown/Source/Unknown/ActivityStations/DefendStation/Monster.cpp
@@ -0,0 +1,26 @@
+﻿#include "Monster.h"
+
+#include "GameFramework/PlayerController.h"
+#include "Unknown/UnknownGameInstance.h"
+#include "Unknown/PlayerCharacter/PCharacter.h"
+#include "Unknown/System/UnknownHUD.h"
+
+AMonster::AMonster()
+{
+	MonsterMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MonsterMeshComponent"));
+	MonsterMeshComponent->SetupAttachment(GetRootComponent());
+
+	MonsterTag = "ToSwat";
+	this->Tags.Add(MonsterTag);
+}
+
+void AMonster::BeginPlay()
+{
+	Super::BeginPlay();
+
+	HUD = Cast<AUnknownHUD>(GetWorld()->GetFirstPlayerController()->GetHUD());
+
+	PlayerCharacter = Cast<APCharacter>(GetWorld()->GetFirstPlayerController()->GetPawn());
+
+	GameInstance = Cast<UUnknownGameInstance>(GetWorld()->GetGameInstance());
+}
---
+++ Unknown/Source/Unknown/ActivityStations/DefendStation/Monster.h
@@ -0,0 +1,45 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Monster.generated.h"
+
+UCLASS()
+class AMonster : public AActor
+{
+	GENERATED_BODY()
+	
+public:
+	// FUNCTIONS
+	// -----------------------------
+	AMonster();
+
+	virtual void BeginPlay() override;
+
+	// PROPERTIES & VARIABLES
+	// -----------------------------
+
+	UPROPERTY()
+	AUnknownHUD* HUD;
+
+	UPROPERTY()
+	APCharacter* PlayerCharacter;
+
+	UPROPERTY()
+	APController* PlayerController;
+
+	UPROPERTY()
+	UUnknownGameInstance* GameInstance;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FName MonsterTag;
+
+protected:
+
+	// PROPERTIES & VARIABLES
+	// -----------------------------
+
+	// It was a mesh!
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UStaticMeshComponent* MonsterMeshComponent;
+	
+};
